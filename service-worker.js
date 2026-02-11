5c5f539d1e9650ff5076e3e128de93087fe13741
chanhazuma <chanhazuma@gmail.com>
2026-02-11 20:29:01 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", () => {
-  console.log("Installed");
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
