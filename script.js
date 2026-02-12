27122e78e5c2c36428e0b637c5a20bf27126a860
chanhazuma <chanhazuma@gmail.com>
2026-02-12 11:15:48 +0700

    update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ alert("js kebaca");
 document.getElementById("down").addEventListener("click", function(e){
  document.getElementById("header").classList.add("headeractive", "jst-l", "pt20", "bg-glass");
  document.getElementById("footer").classList.remove("pss-abs");
- document.getElementById("footer").classList.add("pss-fx");
+ document.getElementById("footer").classList.add("pss-fx","ukr-d-hp", "footeractive");
  document.getElementById("logo").classList.add("logoactive");
 });
 document.getElementById("up").addEventListener("click", function(e){
   document.getElementById("header").classList.remove("headeractive", "jst-l", "pt20", "bg-glass");
   document.getElementById("footer").classList.add("pss-abs");
-  document.getElementById("footer").classList.remove("pss-fx");
+  document.getElementById("footer").classList.remove("pss-fx", "ukr-d-hp", "footeractive");
   document.getElementById("logo").classList.remove("logoactive");
 });
