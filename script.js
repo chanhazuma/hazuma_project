7166ec0e2156b95894ad0df81e458401e844576e
chanhazuma <chanhazuma@gmail.com>
2026-02-12 08:04:50 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 alert("js kebaca");
 let btup = document.getElementById("up");
+console.log(btup);
 let btdown = document.getElementById("down");
 btup.addEventListemer("click", function(e){
  alert("haii");
