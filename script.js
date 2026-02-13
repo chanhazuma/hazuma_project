160f45f987080bdfd895a488ae4efcdae5b59f91
chanhazuma <chanhazuma@gmail.com>
2026-02-13 19:53:58 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 alert("Selamat datang(≧◡≦)💕");
 document.getElementById("down").addEventListener("click", function(e){
- document.getElementById("header").classList.add("headeractive", "jst-l", "pt20", "bg-glass");
+ document.getElementById("header").classList.add("headeractive", "pt20", "bg-glass");
  document.getElementById("footer").classList.remove("pss-abs");
  document.getElementById("footer").classList.add("pss-fx","ukr-d-hp", "footeractive");
  document.getElementById("logo").classList.add("logoactive");
 });
 document.getElementById("up").addEventListener("click", function(e){
-  document.getElementById("header").classList.remove("headeractive", "jst-l", "pt20", "bg-glass");
+  document.getElementById("header").classList.remove("headeractive", "pt20", "bg-glass");
   document.getElementById("footer").classList.add("pss-abs");
   document.getElementById("footer").classList.remove("pss-fx", "ukr-d-hp", "footeractive");
   document.getElementById("logo").classList.remove("logoactive");
