315e4d83d79c88a0789eaa11aa8c05327b2633bd
chanhazuma <chanhazuma@gmail.com>
2026-02-15 17:59:46 +0700

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,9 @@
-const CACHE = "hazuma-pwa-v2";
-
-// install
 self.addEventListener("install", e => {
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", e => {
   e.waitUntil(self.clients.claim());
 });
 
-// fetch
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, copy));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener("fetch", () => {});
