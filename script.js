78e0e9d743659c39b4853a3f6ffc64b8418d5dd9
chanhazuma <chanhazuma@gmail.com>
2026-02-15 17:17:01 +0700

    update script.js

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ document.getElementById("up").addEventListener("click", function(e){
   document.getElementById("footer").classList.remove("pss-fx", "ukr-d-hp", "footeractive");
   document.getElementById("logo").classList.remove("logoactive");
 });
+document.getElementById("wa").addEventListener("click", function(e){
+  window.location.href="https://wa.me/6285747150386"
+});
+document.getElementById("ig").addEventListener("click", function(e){
+  window.location.href="https://www.instagram.com/hanif_zuhud?igsh=MXM5N25uZnVtamRleQ=="
+});
